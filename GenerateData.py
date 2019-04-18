bae2284452afc3ffb0f7ee8bd9e0d9cbfc46b935
Cristiano Firmani <firma103@mail.chapman.edu>
2019-04-17 23:55:10 -0700

    Add files via upload

--- GenerateData.py
+++ GenerateData.py
@@ -5,13 +5,19 @@ import sys
 faker = Faker()
 
 DataArray = []
+TvModels = []
 
-for x in range(0, sys.argv[1]):
+
+for x in range(0, int(sys.argv[2])):
     firstName = (faker.first_name())
     lastName = (faker.last_name())
     streetAddress = (faker.address())
 
     TvModel = rand.randint(1000, 9999)
+    while TvModel not in TvModels:
+        TvModel = rand.randint(1000, 9999)
+        TvModels.append(TvModel)
+
     TvSize = rand.randint(32, 90)
 
     newsLetterBool = bool(rand.getrandbits(1))
@@ -21,4 +27,4 @@ for x in range(0, sys.argv[1]):
     DataArray.insert(x, [firstName, lastName, streetAddress, TvModel, TvSize, newsLetterBool, companyName])
 
     my_df = pd.DataFrame(DataArray)
-    my_df.to_csv(sys.argv[0] + '.csv', index=False, header=False)
+    my_df.to_csv(sys.argv[1] + '.csv', index=False, header=False)
